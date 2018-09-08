0f6b8b9aa3b54dcb801dfed817f0199a60bd3788
Aghathis <aghathis@gmail.com>
2018-09-09 01:27:01 +0200

    Update main.js

--- AllOp/js/main.js
+++ AllOp/js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){ // point de démmarage JS
-	
+$(document).ready(function(){ // point de dÃ©mmarage JS
+	var url = "https://raw.githubusercontent.com/Aghathis/Aghathis.github.io/master/AllOp/Image/";
 	for(i in allGundamOp){
 		var anime = allGundamOp[i];
 		var str = "<div id='animeCard' style='position:relative;padding:50px;'>";
 		str+="<div id='image'";
 		str+="style='z-index:-10;position:absolute;top:0;left:0;right:0;bottom:0;background-size:cover;";
-		str+="filter:blur(5px) brightness(50%);background-image:url(\"image/"+anime.image+"\");'></div>";
+		str+="filter:blur(5px) brightness(50%);background-image:url(\""+url+anime.image+"\");'></div>";
 		str+= "<div id='animeTitle'> - "+anime.anime+" - </div><div id='anime'>";
 		for(j in anime.song){
 			var song = anime.song[j];
