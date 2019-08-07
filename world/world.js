0a2b2e3595f342fcdc8941d601bd60f953520e8e
Aghathis <aghathis@gmail.com>
2019-08-07 17:11:48 +0200

    Add files via upload

---
+++ world/world.js
@@ -0,0 +1,521 @@
+var world = [
+{
+	"nom":"afghanistan",
+	"article":"de l'",
+	"capitale":"kaboul",
+	"langue":["dari","pachto"],
+	"monnaie":["afghani"],
+	"code_iso":"AFG, AF",
+	"drapeau":"9/9a/Flag_of_Afghanistan.svg",
+	"armoirie":"commons/4/47/National_emblem_of_Afghanistan.svg"
+},
+{
+	"nom":"afrique du sud",
+	"article":"de l'",
+	"capitale":"le cap",
+	"langue":["afrikaans","anglais","ndébélé du transvaal","sotho du nord","sotho du sud","swati","tsonga","tswana","venda","xhosa","zoulou"],
+	"monnaie":["rand"],
+	"code_iso":"ZAF, ZA",
+	"drapeau":"a/af/Flag_of_South_Africa.svg",
+	"armoirie":"en/2/21/Coat_of_arms_of_South_Africa.svg"
+},
+{
+	"nom":"albanie",
+	"article":"de l'",
+	"capitale":"tirana",
+	"langue":["albanais"],
+	"monnaie":["lek"],
+	"code_iso":"ALB, AL",
+	"drapeau":"3/36/Flag_of_Albania.svg",
+	"armoirie":"commons/6/6e/Coat_of_arms_of_Albania.svg"
+	},
+{
+	"nom":"algérie",
+	"article":"de l'",
+	"capitale":"alger",
+	"langue":["arabe","tamazight","arabe algérien","français"],
+	"monnaie":["Dinar algérien"],
+	"code_iso":"DZA, DZ",
+	"drapeau":"7/77/Flag_of_Algeria.svg",
+	"armoirie":"commons/d/df/Emblem_of_Algeria.svg"
+},
+{
+	"nom":"allemagne",
+	"article":"de l'",
+	"capitale":"berlin",
+	"langue":["allemand"],
+	"monnaie":["euro","franc suisse"],
+	"code_iso":"DEU, DE",
+	"drapeau":"b/ba/Flag_of_Germany.svg",
+	"armoirie":"commons/d/da/Coat_of_arms_of_Germany.svg"
+},
+{
+	"nom":"andorre",
+	"article":"d'",
+	"capitale":"andore-la-vieille",
+	"langue":["catalan"],
+	"monnaie":["euro"],
+	"code_iso":"AND, AD",
+	"drapeau":"1/19/Flag_of_Andorra.svg",
+	"armoirie":"commons/4/4e/Coat_of_arms_of_Andorra.svg"
+},
+{
+	"nom":"angola",
+	"article":"de l'",
+	"capitale":"luanda",
+	"langue":["portugais"],
+	"monnaie":["kwanza"],
+	"code_iso":"AGO, AO",
+	"drapeau":"9/9d/Flag_of_Angola.svg",
+	"armoirie":"commons/f/ff/Emblem_of_Angola.svg"
+},
+{
+	"nom":"antigua-et-barbuda",
+	"article":"d'",
+	"capitale":"saint john's",
+	"langue":["anglais"],
+	"monnaie":["dollar des caraïbes orientales"],
+	"code_iso":"ATG, AG",
+	"drapeau":"8/89/Flag_of_Antigua_and_Barbuda.svg",
+	"armoirie":"commons/3/30/Coat_of_arms_of_Antigua_and_Barbuda.svg"
+},
+{
+	"nom":"arabie saoudite",
+	"article":"de l'",
+	"capitale":"riyad",
+	"langue":["arabe"],
+	"monnaie":["riyal saoudien"],
+	"code_iso":"SAU, SA",
+	"drapeau":"0/0d/Flag_of_Saudi_Arabia.svg",
+	"armoirie":"commons/9/99/Emblem_of_Saudi_Arabia.svg"
+},
+{
+	"nom":"argentine",
+	"article":"de l'",
+	"capitale":"buenos aires",
+	"langue":["espagnol"],
+	"monnaie":["peso argentin"],
+	"code_iso":"ARG, AR",
+	"drapeau":"1/1a/Flag_of_Argentina.svg",
+	"armoirie":"commons/f/ff/Coat_of_arms_of_Argentina.svg"
+},
+{
+	"nom":"arménie",
+	"article":"de l'",
+	"capitale":"erevan",
+	"langue":["arménien"],
+	"monnaie":["dram"],
+	"code_iso":"ARM, AM",
+	"drapeau":"2/2f/Flag_of_Armenia.svg",
+	"armoirie":"commons/0/0f/Coat_of_arms_of_Armenia.svg"
+},
+{
+	"nom":"australie",
+	"article":"de l'",
+	"capitale":"canberra",
+	"langue":["anglais"],
+	"monnaie":["dollar australien"],
+	"code_iso":"AUS, AU",
+	"drapeau":"8/88/Flag_of_Australia_%28converted%29.svg",
+	"armoirie":"commons/f/f8/Coat_of_Arms_of_Australia.svg"
+},
+{
+	"nom":"autriche",
+	"article":"de l'",
+	"capitale":"vienne",
+	"langue":["allemand"],
+	"monnaie":["euro"],
+	"code_iso":"AUT, AT",
+	"drapeau":"4/41/Flag_of_Austria.svg",
+	"armoirie":"commons/a/a0/Austria_Bundesadler.svg"
+},
+{
+	"nom":"azerbaïdjan",
+	"article":"de l'",
+	"capitale":"bakou",
+	"langue":["azéri"],
+	"monnaie":["manat azerbaïdjanais"],
+	"code_iso":"AZE, AZ",
+	"drapeau":"d/dd/Flag_of_Azerbaijan.svg",
+	"armoirie":"commons/6/6b/Emblem_of_Azerbaijan.svg"
+},
+{
+	"nom":"bahamas",
+	"article":"des ",
+	"capitale":"nasau",
+	"langue":["anglais"],
+	"monnaie":["dollar bahaméen"],
+	"code_iso":"BHS, BS",
+	"drapeau":"9/93/Flag_of_the_Bahamas.svg",
+	"armoirie":"commons/8/8a/Coat_of_arms_of_the_Bahamas.svg"
+},
+{
+	"nom":"bahreïn",
+	"article":"du ",
+	"capitale":"manama",
+	"langue":["arabe"],
+	"monnaie":["dinar bahreïni"],
+	"code_iso":"BHR, BH",
+	"drapeau":"2/2c/Flag_of_Bahrain.svg",
+	"armoirie":"commons/6/6d/Emblem_of_Bahrain.svg"
+},
+{
+	"nom":"bangladesh",
+	"article":"du ",
+	"capitale":"dacca",
+	"langue":["bengali"],
+	"monnaie":["taka"],
+	"code_iso":"BGD,BD",
+	"drapeau":"f/f9/Flag_of_Bangladesh.svg",
+	"armoirie":"commons/8/81/National_emblem_of_Bangladesh.svg"
+},
+{
+	"nom":"barbade",
+	"article":"de la ",
+	"capitale":"bridgetown",
+	"langue":["anglais"],
+	"monnaie":["dollar barbadien"],
+	"code_iso":"BRB, BB",
+	"drapeau":"e/ef/Flag_of_Barbados.svg",
+	"armoirie":"commons/b/bc/Coat_of_arms_of_Barbados_%282%29.svg"
+},
+{
+	"nom":"belgique",
+	"article":"de la ",
+	"capitale":"bruxelles",
+	"langue":["français","néerlandais","allemand"],
+	"monnaie":["euro"],
+	"code_iso":"BEL, BE",
+	"drapeau":"6/65/Flag_of_Belgium.svg",
+	"armoirie":"commons/f/f1/Great_coat_of_arms_of_Belgium.svg"
+},
+{
+	"nom":"belize",
+	"article":"du ",
+	"capitale":"belmopan",
+	"langue":["anglais"],
+	"monnaie":["dollar bélizien"],
+	"code_iso":"BLZ, BZ",
+	"drapeau":"e/e7/Flag_of_Belize.svg",
+	"armoirie":"commons/d/dd/Coat_of_arms_of_Belize.svg"
+},
+{
+	"nom":"bénin",
+	"article":"du ",
+	"capitale":"porto-novo",
+	"langue":["français"],
+	"monnaie":["franc cfa"],
+	"code_iso":"BEN, BJ",
+	"drapeau":"0/0a/Flag_of_Benin.svg",
+	"armoirie":"commons/1/13/Coat_of_arms_of_Benin.svg"
+},
+{
+	"nom":"bhoutan",
+	"article":"du ",
+	"capitale":"thimphou",
+	"langue":["dzongkha"],
+	"monnaie":["ngultrum"],
+	"code_iso":"BTN, BT",
+	"drapeau":"9/91/Flag_of_Bhutan.svg",
+	"armoirie":"commons/e/eb/Emblem_of_Bhutan.svg"
+},
+{
+	"nom":"biélorussie",
+	"article":"de la ",
+	"capitale":"minsk",
+	"langue":["biélorusse","russe"],
+	"monnaie":["rouble biélorusse"],
+	"code_iso":"BLR, BY",
+	"drapeau":"8/85/Flag_of_Belarus.svg",
+	"armoirie":"commons/b/bc/Coat_of_arms_of_Belarus_%28official%29.svg"
+},
+{
+	"nom":"birmanie",
+	"article":"de la ",
+	"capitale":"naypyidaw",
+	"langue":["birman"],
+	"monnaie":["kyat"],
+	"code_iso":"MMR, MM",
+	"drapeau":"8/8c/Flag_of_Myanmar.svg",
+	"armoirie":"commons/8/89/State_seal_of_Myanmar.svg"
+},
+{
+	"nom":"bolivie",
+	"article":"de la ",
+	"capitale":"la paz",
+	"langue":["quechua","guarani","aymara","espagnol"],
+	"monnaie":["boliviano"],
+	"code_iso":"BOL, BO",
+	"drapeau":"4/48/Flag_of_Bolivia.svg",
+	"armoirie":"commons/0/06/Coat_of_arms_of_Bolivia.svg"
+},
+{
+	"nom":"bosnie-herzégovine",
+	"article":"de la ",
+	"capitale":"sarajevo",
+	"langue":["bosnien","serbe","croate"],
+	"monnaie":["mark convertible"],
+	"code_iso":"BIH, BA",
+	"drapeau":"b/bf/Flag_of_Bosnia_and_Herzegovina.svg",
+	"armoirie":"commons/d/d4/Coat_of_arms_of_Bosnia_and_Herzegovina.svg"
+},
+{
+	"nom":"botswana",
+	"article":"du ",
+	"capitale":"gaborone",
+	"langue":["tswana","anglais"],
+	"monnaie":["pula"],
+	"code_iso":"BWA, BW",
+	"drapeau":"f/fa/Flag_of_Botswana.svg",
+	"armoirie":"commons/d/de/Coat_of_arms_of_Botswana.svg"
+},
+{
+	"nom":"brésil",
+	"article":"du ",
+	"capitale":"brasilia",
+	"langue":["portugais"],
+	"monnaie":["réal"],
+	"code_iso":"BRA, BR",
+	"drapeau":"0/05/Flag_of_Brazil.svg",
+	"armoirie":"commons/b/bf/Coat_of_arms_of_Brazil.svg"
+},
+{
+	"nom":"brunei",
+	"article":"du ",
+	"capitale":"bandar seri begawan",
+	"langue":["malais"],
+	"monnaie":["dollar de brunei"],
+	"code_iso":"BRN, BN",
+	"drapeau":"9/9c/Flag_of_Brunei.svg",
+	"armoirie":"commons/0/09/Emblem_of_Brunei.svg"
+},
+{
+	"nom":"bulgarie",
+	"article":"de la ",
+	"capitale":"sofia",
+	"langue":["bulgare"],
+	"monnaie":["lev"],
+	"code_iso":"BGR, BG",
+	"drapeau":"9/9a/Flag_of_Bulgaria.svg",
+	"armoirie":"commons/2/24/Coat_of_arms_of_Bulgaria.svg"
+},
+{
+	"nom":"burkina faso",
+	"article":"du ",
+	"capitale":"ouagadougou",
+	"langue":["français"],
+	"monnaie":["franc cfa"],
+	"code_iso":"BFA, BF",
+	"drapeau":"3/31/Flag_of_Burkina_Faso.svg",
+	"armoirie":"commons/a/a8/Burkina_Faso_COA.png"
+},
+{
+	"nom":"burundi",
+	"article":"du ",
+	"capitale":"gitega",
+	"langue":["français,kirundi"],
+	"monnaie":["franc burundais"],
+	"code_iso":"BDI, BI",
+	"drapeau":"5/50/Flag_of_Burundi.svg",
+	"armoirie":"commons/4/4b/Blason_du_Burundi.svg"
+},
+{
+	"nom":"cambodge",
+	"article":"du ",
+	"capitale":"phnom penh",
+	"langue":["khmer"],
+	"monnaie":["riel"],
+	"code_iso":"KHM, KH",
+	"drapeau":"8/83/Flag_of_Cambodia.svg",
+	"armoirie":"commons/1/1e/Royal_arms_of_Cambodia.svg"
+},
+{
+	"nom":"cameroun",
+	"article":"du ",
+	"capitale":"yaoundé",
+	"langue":["français","anglais"],
+	"monnaie":["franc cfa"],
+	"code_iso":"CMR, CM",
+	"drapeau":"4/4f/Flag_of_Cameroon.svg",
+	"armoirie":"commons/b/b5/Coat_of_arms_of_Cameroon.svg"
+},
+{
+	"nom":"canada",
+	"article":"du ",
+	"capitale":"ottawa",
+	"langue":["français","anglais"],
+	"monnaie":["dollar canadien"],
+	"code_iso":"CAN, CA",
+	"drapeau":"d/d9/Flag_of_Canada_%28Pantone%29.svg",
+	"armoirie":"commons/c/c5/Coat_of_arms_of_Canada_rendition.svg"
+},
+{
+	"nom":"cap-vert",
+	"article":"du ",
+	"capitale":"praia",
+	"langue":["portugais"],
+	"monnaie":["escudo cap-verdien"],
+	"code_iso":"CPV, CV",
+	"drapeau":"3/38/Flag_of_Cape_Verde.svg",
+	"armoirie":"commons/c/ce/Coat_of_arms_of_Cape_Verde.svg"
+},
+{
+	"nom":"république centrafricaine",
+	"article":"de la ",
+	"capitale":"bangui",
+	"langue":["français","sango"],
+	"monnaie":["franc cfa"],
+	"code_iso":"CAF, CF",
+	"drapeau":"6/6f/Flag_of_the_Central_African_Republic.svg",
+	"armoirie":"commons/0/0a/Coat_of_arms_of_the_Central_African_Republic.svg"
+},
+{
+	"nom":"chili",
+	"article":"du ",
+	"capitale":"santiago",
+	"langue":["espagnol"],
+	"monnaie":["peso chilien"],
+	"code_iso":"CHL, CL",
+	"drapeau":"7/78/Flag_of_Chile.svg",
+	"armoirie":"commons/5/5b/Coat_of_arms_of_Chile.svg"
+},
+{
+	"nom":"chine",
+	"article":"de la ",
+	"capitale":"pékin",
+	"langue":["chinois mandarin"],
+	"monnaie":["yuan","renminbi"],
+	"code_iso":"CHN, CN",
+	"drapeau":"f/fa/Flag_of_the_People%27s_Republic_of_China.svg",
+	"armoirie":"commons/5/55/National_Emblem_of_the_People%27s_Republic_of_China.svg"
+},
+{
+	"nom":"chypre",
+	"article":"de ",
+	"capitale":"nicosie",
+	"langue":["grec","turc"],
+	"monnaie":["euro"],
+	"code_iso":"CYP, CY",
+	"drapeau":"d/d4/Flag_of_Cyprus.svg",
+	"armoirie":"en/9/9d/Coat_of_Arms_of_Cyprus-_Official_Depiction.svg"
+},
+{
+	"nom":"colombie",
+	"article":"de la ",
+	"capitale":"bogota",
+	"langue":["espagnol"],
+	"monnaie":["peso colombien"],
+	"code_iso":"COL, CO",
+	"drapeau":"2/21/Flag_of_Colombia.svg",
+	"armoirie":"commons/e/ef/Coat_of_arms_of_Colombia.svg"
+},
+{
+	"nom":"comores",
+	"article":"des ",
+	"capitale":"moroni",
+	"langue":["comorien","français","arabe"],
+	"monnaie":["franc comorien"],
+	"code_iso":"COM, KM",
+	"drapeau":"9/94/Flag_of_the_Comoros.svg",
+	"armoirie":"commons/8/8c/Seal_of_the_Comoros.svg"
+},
+{
+	"nom":"république du congo",
+	"article":"de la ",
+	"capitale":"brazzaville",
+	"langue":["français","kituba","lingala"],
+	"monnaie":["franc cfa"],
+	"code_iso":"COG, CG",
+	"drapeau":"9/92/Flag_of_the_Republic_of_the_Congo.svg",
+	"armoirie":"commons/d/d8/Coat_of_arms_of_the_Republic_of_the_Congo.svg"
+},
+{
+	"nom":"république démocratique du congo",
+	"article":"de la ",
+	"capitale":"kinshasa",
+	"langue":["français","lingala","swahili","tshiluba"],
+	"monnaie":["franc congolais"],
+	"code_iso":"COD, CD",
+	"drapeau":"6/6f/Flag_of_the_Democratic_Republic_of_the_Congo.svg",
+	"armoirie":"commons/1/1b/Coat_of_arms_of_the_Democratic_Republic_of_the_Congo.svg"
+},
+{
+	"nom":"îles cook",
+	"article":"des ",
+	"capitale":"avarua",
+	"langue":["anglais","maori des îles"],
+	"monnaie":["dollar des îles cook","dollar néo-zélandais"],
+	"code_iso":"COK, CK",
+	"drapeau":"3/35/Flag_of_the_Cook_Islands.svg",
+	"armoirie":"commons/c/c2/Coat_of_arms_of_the_Cook_Islands.svg"
+},
+{
+	"nom":"corée du nord",
+	"article":"de la ",
+	"capitale":"pyongyang",
+	"langue":["coréen"],
+	"monnaie":["won nord-coréen"],
+	"code_iso":"PRK, KP",
+	"drapeau":"5/51/Flag_of_North_Korea.svg",
+	"armoirie":"commons/3/3c/Emblem_of_North_Korea.svg"
+},
+{
+	"nom":"corée du sud",
+	"article":"de la ",
+	"capitale":"séoul",
+	"langue":["coréen"],
+	"monnaie":["won sud-coréen"],
+	"code_iso":"KOR, KR",
+	"drapeau":"0/09/Flag_of_South_Korea.svg",
+	"armoirie":"commons/9/96/Emblem_of_South_Korea.svg"
+},
+{
+	"nom":"costa rica",
+	"article":"du ",
+	"capitale":"san josé",
+	"langue":["espagnol","mekatelyu","bribri"],
+	"monnaie":["colón"],
+	"code_iso":"CRI, CR",
+	"drapeau":"f/f2/Flag_of_Costa_Rica.svg",
+	"armoirie":"commons/8/84/Coat_of_arms_of_Costa_Rica.svg"
+},
+{
+	"nom":"côte d'ivoire",
+	"article":"de la ",
+	"capitale":"yamoussoukro",
+	"langue":["français"],
+	"monnaie":["franc cfa"],
+	"code_iso":"CIV, CI",
+	"drapeau":"f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg",
+	"armoirie":"commons/9/95/Coat_of_arms_of_Ivory_Coast.svg"
+},
+{
+	"nom":"croatie",
+	"article":"de la ",
+	"capitale":"zagreb",
+	"langue":["croate"],
+	"monnaie":["kuna"],
+	"code_iso":"HRV, HR",
+	"drapeau":"1/1b/Flag_of_Croatia.svg",
+	"armoirie":"commons/c/c9/Coat_of_arms_of_Croatia.svg"
+},
+{
+	"nom":"cuba",
+	"article":"de ",
+	"capitale":"la havane",
+	"langue":["espagnol"],
+	"monnaie":["peso cubain convertible"],
+	"code_iso":"CUB, CU",
+	"drapeau":"b/bd/Flag_of_Cuba.svg",
+	"armoirie":"commons/5/58/Coat_of_arms_of_Cuba.svg"
+},
+{
+	"nom":"",
+	"capitale":"",
+	"langue":[""],
+	"monnaie":[""],
+	"code_iso":"",
+	"drapeau":"",
+	"armoirie":""
+}
+]
