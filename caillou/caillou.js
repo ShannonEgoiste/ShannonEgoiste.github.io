2ffa92958b668efd84651c57d34ac5e9f446ceb7
Aghathis <aghathis@gmail.com>
2021-04-01 04:34:48 +0200

    Add files via upload

---
+++ caillou/caillou.js
@@ -0,0 +1,134 @@
+var caillou = [
+  {
+    "Nom": "Asscher",
+    "Cotation": [
+      "C",
+      "l"
+    ]
+  },
+  {
+    "Nom": "Baguette",
+    "Cotation": [
+      "L",
+      "l"
+    ]
+  },
+  {
+    "Nom": "Brillant",
+    "Cotation": [
+      "Diametre"
+    ]
+  },
+  {
+    "Nom": "Calf",
+    "Cotation": [
+      "L",
+	  "l",
+	  "C",
+	  "C'"
+    ]
+  },
+  {
+    "Nom": "Coeur",
+    "Cotation": [
+      "L",
+	  "l"
+    ]
+  },
+  {
+    "Nom": "Coussin carre",
+    "Cotation": [
+      "L",
+	 ]
+  },
+  {
+    "Nom": "Coussin Rectangle",
+    "Cotation": [
+      "L",
+	  "l",
+	 ]
+  },
+  {
+    "Nom": "Demi Lune",
+    "Cotation": [
+      "L",
+	  "l",
+	 ]
+  },
+  {
+    "Nom": "Emeraude",
+    "Cotation": [
+      "L",
+	  "l",
+	  "C"
+	 ]
+  },
+  {
+    "Nom": "Marquise",
+    "Cotation": [
+      "L",
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Octogone",
+    "Cotation": [
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Ovale",
+    "Cotation": [
+	  "L",
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Perle",
+    "Cotation": [
+	  "Diametre"
+	 ]
+  },
+  {
+    "Nom": "Pierre Diforme et Cabochon",
+    "Cotation": [
+	  "L",
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Poire",
+    "Cotation": [
+	  "L",
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Princesse",
+    "Cotation": [
+	  "L"
+	 ]
+  },
+  {
+    "Nom": "Tapers",
+    "Cotation": [
+	  "L",
+	  "l",
+	  "L'"
+	 ]
+  },
+  {
+    "Nom": "Troidia coussin",
+    "Cotation": [
+	  "L",
+	  "l"
+	 ]
+  },
+  {
+    "Nom": "Troidia",
+    "Cotation": [
+	  "L",
+	  "l"
+	 ]
+  }
+]
