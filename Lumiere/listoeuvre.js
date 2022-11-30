67c771b53ade33a0e2620ffc5a9f2b9bc9a9e4a6
ShannonEgoiste <aghathis@gmail.com>
2022-11-30 16:36:07 +0100

    Add files via upload

---
+++ Lumiere/listoeuvre.js
@@ -0,0 +1,182 @@
+var listoeuvre = [
+    {
+        "name": "Time",
+        "adress": "Cathédrale Saint-Jean",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-11/Time_ROCA.jpg",
+        "numero": 1
+    },
+    {
+        "name": "Le Voyageur Céleste",
+        "adress": "Colline de Fourvière",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Le-voyageur-c%C3%A9leste.jpg",
+        "numero": 2
+    },
+    {
+        "name": "Inside / Out",
+        "adress": "Place du Change",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Inside-out.jpg",
+        "numero": 3
+    },
+    {
+        "name": "Nouvelle Vague ",
+        "adress": "Gare Saint-Paul",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Nouvelle-vague.jpg",
+        "numero": 4
+    },
+    {
+        "name": "Le Bal Lumière",
+        "adress": "Les Subs",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Le-bal-lumi%C3%A8re.jpg",
+        "numero": 5
+    },
+    {
+        "name": "Expérimentations étudiantes",
+        "adress": "Place Sathonay",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Exp%C3%A9rimentations-%C3%A9tudiantes_1.jpg",
+        "numero": 6
+    },
+    {
+        "name": "Grand Mix au Musée des Beaux-Arts de Lyon",
+        "adress": "Place des Terreaux",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Grand-mix-au-mus%C3%A9e-des-beaux-arts-de-lyon_0.jpg",
+        "numero": 7
+    },
+    {
+        "name": "UNICODE光3D",
+        "adress": "Place Louis Pradel",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Unicode%E5%85%893D.jpg",
+        "numero": 8
+    },
+    {
+        "name": "Eikosis",
+        "adress": "Place de la Bourse",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Eikosi_0.jpg",
+        "numero": 9
+    },
+    {
+        "name": "Planétoïdes",
+        "adress": "Rue de la République",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Planeto%C3%AFdes_0.jpg",
+        "numero": 10
+    },
+    {
+        "name": "J'ai attrapé un...",
+        "adress": "Quartier Grolée",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/J%27ai-attrap%C3%A9-un_0.jpg",
+        "numero": 11
+    },
+    {
+        "name": "Cymopolée",
+        "adress": "Place de la République",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Cymopol%C3%A9e.jpg",
+        "numero": 12
+    },
+    {
+        "name": "Murmuration",
+        "adress": "Place des Jacobins",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Murmuration_0.jpg",
+        "numero": 13
+    },
+    {
+        "name": "Menschen",
+        "adress": "Place des Célestins",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Menschen_0.jpg",
+        "numero": 14
+    },
+    {
+        "name": "I ♥ LIGHT",
+        "adress": "Place Bellecour",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-11/i%20love%20light-Filsdecre%CC%81a-FDL22-RVB.jpg",
+        "numero": 15
+    },
+    {
+        "name": "Mirror Mountain",
+        "adress": "Place Bellecour",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Mirror-mountain.jpg",
+        "numero": 16
+    },
+    {
+        "name": "Soi-même",
+        "adress": "Fondation Bullukian",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Soi-m%C3%AAme.jpg",
+        "numero": 17
+    },
+    {
+        "name": "The Shape of Things to Come",
+        "adress": "Place Antonin Poncet",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/The-shape-of-things-to-come.jpg",
+        "numero": 18
+    },
+    {
+        "name": "Alcove LTD",
+        "adress": "Berges du Rhône",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Alcove-LTD_0.jpg",
+        "numero": 19
+    },
+    {
+        "name": "AGORYTHM",
+        "adress": "Parc de la Tête d'Or",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Agorythm_0.jpg",
+        "numero": 20
+    },
+    {
+        "name": "Firefly Field",
+        "adress": "Parc de la Tête d'Or",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Firefly-field_1.jpg",
+        "numero": 21
+    },
+    {
+        "name": "Le Parc du Nombre d'Or ",
+        "adress": "Parc de la Tête d'Or",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Le-parc-du-nombre-d%27or_0.jpg",
+        "numero": 22
+    },
+    {
+        "name": "Les Lumignons du Cœur ",
+        "adress": "Parc de la Tête d'Or",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Les-lumignons-du-coeur.jpg",
+        "numero": 23
+    },
+    {
+        "name": "Urban Oracle",
+        "adress": "Allée Achille Lignon",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Urban-oracle.jpg",
+        "numero": 24
+    },
+    {
+        "name": "Groovy Bibli",
+        "adress": "Bibliothèque municipale de la Part-Dieu",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Groovy-bibli_1.jpg",
+        "numero": 25
+    },
+    {
+        "name": "Gazouillis",
+        "adress": "Place Voltaire",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Gazouillis_0.jpg",
+        "numero": 26
+    },
+    {
+        "name": "Luminous Book - \"La Manu à livre ouvert\"",
+        "adress": "Manufacture des Tabacs",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Luminous-book-la-manu-a-ciel-ouvert.jpg",
+        "numero": 27
+    },
+    {
+        "name": "Beacon",
+        "adress": "Jardin de l'Institut Lumière",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Beacon.jpg",
+        "numero": 28
+    },
+    {
+        "name": "Cachés dans la ville",
+        "adress": "Parc Blandan",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-11/Cach%C3%A9s-dans-la-ville_0.jpg",
+        "numero": 29
+    },
+    {
+        "name": "Pulse",
+        "adress": "Centre Hospitalier Saint-Joseph Saint-Luc",
+        "image": "https://www.fetedeslumieres.lyon.fr/sites/fdl/files/2022-10/Pulse.jpg",
+        "numero": 30
+    }
+]
