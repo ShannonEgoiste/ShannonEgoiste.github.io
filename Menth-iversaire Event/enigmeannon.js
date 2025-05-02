4060b5ba094139803ddebcc60a70162c72e9a252
ShannonEgoiste <aghathis@gmail.com>
2025-05-02 12:32:28 +0200

    Menthe-iversaire app

---
+++ Menth-iversaire Event/enigmeannon.js
@@ -0,0 +1,352 @@
+var listEnigme = [
+  {
+    "n": "1",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "1"
+  },
+  {
+    "n": "2",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "3",
+    "expac": "HW",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "4",
+    "expac": "HW",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "5",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "1"
+  },
+  {
+    "n": "6",
+    "expac": "HW",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "1"
+  },
+  {
+    "n": "7",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "8",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "9",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Objet",
+    "point": "1"
+  },
+  {
+    "n": "10",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "11",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "1"
+  },
+  {
+    "n": "12",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Objet",
+    "point": "1"
+  },
+  {
+    "n": "13",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "1"
+  },
+  {
+    "n": "14",
+    "expac": "Menthe",
+    "enigme": "xxxxxxxx",
+    "subject": "Menthe",
+    "point": "1"
+  },
+  {
+    "n": "15",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "2"
+  },
+  {
+    "n": "16",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "2"
+  },
+  {
+    "n": "17",
+    "expac": "ShB",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "2"
+  },
+  {
+    "n": "18",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "2"
+  },
+  {
+    "n": "19",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "2"
+  },
+  {
+    "n": "20",
+    "expac": "StB",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "2"
+  },
+  {
+    "n": "21",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "2"
+  },
+  {
+    "n": "22",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "2"
+  },
+  {
+    "n": "23",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "2"
+  },
+  {
+    "n": "24",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Objet",
+    "point": "2"
+  },
+  {
+    "n": "25",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "2"
+  },
+  {
+    "n": "26",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "2"
+  },
+  {
+    "n": "27",
+    "expac": "Menthe",
+    "enigme": "xxxxxxxx",
+    "subject": "Menthe",
+    "point": "2"
+  },
+  {
+    "n": "28",
+    "expac": "Menthe",
+    "enigme": "xxxxxxxx",
+    "subject": "Menthe",
+    "point": "2"
+  },
+  {
+    "n": "29",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "30",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "3"
+  },
+  {
+    "n": "31",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "32",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "33",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "3"
+  },
+  {
+    "n": "34",
+    "expac": "ARR ou ShB",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "35",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "3"
+  },
+  {
+    "n": "36",
+    "expac": "StB",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "37",
+    "expac": "ShB",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "38",
+    "expac": "HW",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "3"
+  },
+  {
+    "n": "39",
+    "expac": "HW",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "3"
+  },
+  {
+    "n": "40",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "3"
+  },
+  {
+    "n": "41",
+    "expac": "Menthe",
+    "enigme": "xxxxxxxx",
+    "subject": "Menthe",
+    "point": "3"
+  },
+  {
+    "n": "42",
+    "expac": "StB",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "3"
+  },
+  {
+    "n": "43",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "5"
+  },
+  {
+    "n": "44",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "PNJ",
+    "point": "5"
+  },
+  {
+    "n": "45",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "5"
+  },
+  {
+    "n": "46",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "5"
+  },
+  {
+    "n": "47",
+    "expac": "Toute",
+    "enigme": "xxxxxxxx",
+    "subject": "Joueur",
+    "point": "5"
+  },
+  {
+    "n": "48",
+    "expac": "ShB",
+    "enigme": "xxxxxxxx",
+    "subject": "Instance",
+    "point": "5"
+  },
+  {
+    "n": "49",
+    "expac": "ARR",
+    "enigme": "xxxxxxxx",
+    "subject": "Lieu",
+    "point": "5"
+  },
+  {
+    "n": "50",
+    "expac": "Menthe",
+    "enigme": "xxxxxxxx",
+    "subject": "Menthe",
+    "point": "5"
+  }
+]
