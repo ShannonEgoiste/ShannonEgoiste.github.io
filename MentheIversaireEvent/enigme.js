30ad2a90eca99ab6325c18869408c4005529a34d
unknown <aghathis@gmail.com>
2025-05-05 22:54:02 +0200

    c'est mon anniv :D

--- MentheIversaireEvent/enigme.js
+++ MentheIversaireEvent/enigme.js
@@ -2,350 +2,350 @@ var listEnigme = [
   {
     "n": "1",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Kupo !",
     "subject": "PNJ",
     "point": "1"
   },
   {
     "n": "2",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Comme à Noël sous le sapin",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "3",
     "expac": "HW",
-    "enigme": "xxxxxxxx",
+    "enigme": "Cette île ressemble à un coeur...",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "4",
     "expac": "HW",
-    "enigme": "xxxxxxxx",
+    "enigme": "...et cette île la à un mog !",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "5",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Bunnygirl qui faigneantise",
     "subject": "PNJ",
     "point": "1"
   },
   {
     "n": "6",
     "expac": "HW",
-    "enigme": "xxxxxxxx",
+    "enigme": "Une Miqo'te sur un marchepieds qui nous casse les pieds",
     "subject": "PNJ",
     "point": "1"
   },
   {
     "n": "7",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "On peut y récolter du Terreau !",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "8",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Vous avez remarquer ? On y commence quasi tous les chapitres de la MSQ",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "9",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "On a tous un objet qui rapporterais BEAUCOUP si on le vendais !",
     "subject": "Objet",
     "point": "1"
   },
   {
     "n": "10",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "La 7.2 nous a privé l'accès d'un endroit très précieux...",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "11",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "\"My heart is dragging me down into oblivion\"",
     "subject": "Instance",
     "point": "1"
   },
   {
     "n": "12",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Il en faut 99 pour une monture...",
     "subject": "Objet",
     "point": "1"
   },
   {
     "n": "13",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Quel émotion, vous venez de débloquer votre tout premier donjon !",
     "subject": "Lieu",
     "point": "1"
   },
   {
     "n": "14",
     "expac": "Menthe",
-    "enigme": "xxxxxxxx",
+    "enigme": "La vue est à \"COUPE\"r le souffle",
     "subject": "Menthe",
     "point": "1"
   },
   {
     "n": "15",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Un jeu de plateau... Mais pas de cartes",
     "subject": "Lieu",
     "point": "2"
   },
   {
     "n": "16",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Un batiment \"Art Nouveau\"",
     "subject": "Lieu",
     "point": "2"
   },
   {
     "n": "17",
     "expac": "ShB",
-    "enigme": "xxxxxxxx",
+    "enigme": "Une ville \"Art Déco\"",
     "subject": "Lieu",
     "point": "2"
   },
   {
     "n": "18",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "C'est fou comment beaucoup de gens on des noms de nourriture !",
     "subject": "Joueur",
     "point": "2"
   },
   {
     "n": "19",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Cette personne a une relique !",
     "subject": "Joueur",
     "point": "2"
   },
   {
     "n": "20",
     "expac": "StB",
-    "enigme": "xxxxxxxx",
+    "enigme": "Une référence à FFVI",
     "subject": "Instance",
     "point": "2"
   },
   {
     "n": "21",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Les Tarutaru on le nez foncé, certains Lala aussi !",
     "subject": "Joueur",
     "point": "2"
   },
   {
     "n": "22",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Transcendance 3 !",
     "subject": "Instance",
     "point": "2"
   },
   {
     "n": "23",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Il y en a douze, trouver un de leurs insigne !",
     "subject": "Lieu",
     "point": "2"
   },
   {
     "n": "24",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Une Référence à FFIX",
     "subject": "Objet",
     "point": "2"
   },
   {
     "n": "25",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Trop de miqo'te... Trouver un Elezen crépusculaire",
     "subject": "Joueur",
     "point": "2"
   },
   {
     "n": "26",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "\"Donne moi un Elixir\"",
     "subject": "PNJ",
     "point": "2"
   },
   {
     "n": "27",
     "expac": "Menthe",
-    "enigme": "xxxxxxxx",
+    "enigme": "<a href='img/MentheBirthdayCake.jpg' target='_blank' rel='noopener noreferrer'>Où est mon gâteau d'anniversaire ?</a>",
     "subject": "Menthe",
     "point": "2"
   },
   {
     "n": "28",
     "expac": "Menthe",
-    "enigme": "xxxxxxxx",
+    "enigme": "Le nom de mon Choco !",
     "subject": "Menthe",
     "point": "2"
   },
   {
     "n": "29",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "On peut plonger sous l'eau dans ARR ???",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "30",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Cette arme ultime brille d'un magnifique bleu",
     "subject": "Joueur",
     "point": "3"
   },
   {
     "n": "31",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Le Gold Saucer !... Mais vue de l'extérieur !",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "32",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Le Haute voltige de et 20... ",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "33",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Il juge vos glam une fois par semaine... Mais sans son masque !",
     "subject": "PNJ",
     "point": "3"
   },
   {
     "n": "34",
     "expac": "ARR ou ShB",
-    "enigme": "xxxxxxxx",
+    "enigme": "C'est pas les référence à FFIII qui manque ! Celle ci touche les cieux",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "35",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Il en faut pour tout les gouts... mais d'ici à se trimballer un pâté de boue...",
     "subject": "Joueur",
     "point": "3"
   },
   {
     "n": "36",
     "expac": "StB",
-    "enigme": "xxxxxxxx",
+    "enigme": "C'est pas le titanic... Mais il a fait naufrage",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "37",
     "expac": "ShB",
-    "enigme": "xxxxxxxx",
+    "enigme": "Au pied de la tour de cristal",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "38",
     "expac": "HW",
-    "enigme": "xxxxxxxx",
+    "enigme": "Le Zenith mais dorée",
     "subject": "Instance",
     "point": "3"
   },
   {
     "n": "39",
     "expac": "HW",
-    "enigme": "xxxxxxxx",
+    "enigme": "<a href='https://www.youtube.com/watch?v=ijwe2F-Sy1E' target='_blank' rel='noopener noreferrer'>https://www.youtube.com/watch?v=ijwe2F-Sy1E</a>",
     "subject": "PNJ",
     "point": "3"
   },
   {
     "n": "40",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Certains les trouves mignon... moi j'ai définitivement peur de me faire poignarder...",
     "subject": "Instance",
     "point": "3"
   },
   {
     "n": "41",
     "expac": "Menthe",
-    "enigme": "xxxxxxxx",
+    "enigme": "Tic tac. Tic tac.. le temps défile",
     "subject": "Menthe",
     "point": "3"
   },
   {
     "n": "42",
     "expac": "StB",
-    "enigme": "xxxxxxxx",
+    "enigme": "En haut de la tour de Kugane",
     "subject": "Lieu",
     "point": "3"
   },
   {
     "n": "43",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Elle vends du Roquefort ??",
     "subject": "PNJ",
     "point": "5"
   },
   {
     "n": "44",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "Beaucoup de palefrenier mais très peu de Lalafell parmis eux",
     "subject": "PNJ",
     "point": "5"
   },
   {
     "n": "45",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "On se croirait dans une arène Pokemon ! Mais il n'y a que des Carbuncle",
     "subject": "Lieu",
     "point": "5"
   },
   {
     "n": "46",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "Parfois on gagne à être gentils ! offre une mascotte à quelqu'un",
     "subject": "Joueur",
     "point": "5"
   },
   {
     "n": "47",
     "expac": "Toute",
-    "enigme": "xxxxxxxx",
+    "enigme": "La preuve que cette personne joue depuis la 1.0",
     "subject": "Joueur",
     "point": "5"
   },
   {
     "n": "48",
     "expac": "ShB",
-    "enigme": "xxxxxxxx",
+    "enigme": "Une Référence... \"pixeliser\" à Final Fantasy I !",
     "subject": "Instance",
     "point": "5"
   },
   {
     "n": "49",
     "expac": "ARR",
-    "enigme": "xxxxxxxx",
+    "enigme": "A Limsa on peut devenir : Maraudeur, Arcaniste, Surineur ET... Boucanier ???",
     "subject": "Lieu",
     "point": "5"
   },
   {
     "n": "50",
     "expac": "Menthe",
-    "enigme": "xxxxxxxx",
+    "enigme": "Reproduisait mon premier <a target='_blank' rel='noopener noreferrer' href='img/Menthe First.jpg'>screenshot</a> !",
     "subject": "Menthe",
     "point": "5"
   }
